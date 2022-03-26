c9f84b8fab415c62fdd35d9e14943a0db6acb0bc
emcjava <loyusum246807952@gmail.com>
2022-03-26 21:02:31 +0800

    Added: Box collider for house

--- Assets/Script/ConvexConfigurator.cs
+++ Assets/Script/ConvexConfigurator.cs
@@ -5,7 +5,14 @@ public class ConvexConfigurator : MonoBehaviour
 {
     private void OnEnable()
     {
+        var a = FindObjectsOfType(typeof(Component)).ToList().Select(stat => ((Component)stat).gameObject).TakeWhile(t => !t.CompareTag("NotConvex"));
+        
+        print("running");
         foreach (var t1 in FindObjectsOfType(typeof(Component)).ToList().Select(stat => ((Component)stat).gameObject).TakeWhile(t => !t.CompareTag("NotConvex")).Select(t => t.GetComponentsInChildren<MeshCollider>(true))
-                     .SelectMany(allComponents => allComponents)) t1.convex = true;
+                     .SelectMany(allComponents => allComponents).TakeWhile(t => !t.convex))
+        {
+            print("Changing " + t1.name + "to convex.");
+            t1.convex = true;
+        }
     }
 }
