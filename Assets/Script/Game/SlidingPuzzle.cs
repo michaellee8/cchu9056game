21af4c59345e3cd6c16358c5503743bd12c9116f
emcjava <loyusum246807952@gmail.com>
2022-04-15 22:25:53 +0800

    Added playable slide puzzle

--- Assets/Script/Game/SlidingPuzzle.cs
+++ Assets/Script/Game/SlidingPuzzle.cs
@@ -13,7 +13,7 @@ namespace Script.Game
     public class LocationMeta : Object
     {
         public Vector3Int CurrentLocation;
-        private readonly Vector3Int DefaultLocation;
+        public Vector3Int DefaultLocation;
 
         public LocationMeta(Vector3Int currentLocation, Vector3Int defaultLocation)
         {
@@ -27,13 +27,41 @@ namespace Script.Game
         }
     }
 
+    [Serializable]
+    public class TransformData
+    {
+        public Vector3 position = Vector3.zero;
+        public Vector3 eulerRotation = Vector3.zero;
+        public Vector3 scale = Vector3.one;
+
+        // Unity requires a default constructor for serialization
+        public TransformData() { }
+
+        public TransformData(Transform transform)
+        {
+            position = transform.position;
+            eulerRotation = transform.eulerAngles;
+            scale = transform.localScale;
+        }
+
+        public void ApplyTo(Transform transform)
+        {
+            transform.position = position;
+            transform.eulerAngles = eulerRotation;
+            // transform.localScale = LocalScale;
+        }
+    }
     public class SlidingPuzzle : MonoBehaviour
     {
 
-        [SerializeField] private GameObject parentObject;
-        [SerializeField] private GameObject mainMesh;
+        public TransformData finalTransform;
+        private Vector3 finalScale;
+        private Bounds meshBound;
+        public GameObject parentObject;
+        public GameObject mainMesh;
         [SerializeField] private Material crossSectionMaterial;
 
+        private Vector3 _mainMeshSlidedSize;
         private Vector3 _mainMeshScale;
 
         [SerializeField] private Vector3Int finalDimension;
@@ -42,6 +70,8 @@ namespace Script.Game
         private Vector3Int _emptySpace = Vector3Int.zero;
         private Vector3 _midVertex;
 
+        private bool gameover = false;
+        
         private IEnumerable<GameObject> Slice(GameObject obj, Vector3 begin, Vector3 step, int count)
         {
             var result = new List<GameObject>();
@@ -101,39 +131,59 @@ namespace Script.Game
             return result;
         }
 
-        private void Start()
+        private IEnumerator SliceThread()
         {
-            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+            parentObject = new GameObject("FacePuzzleParent");
+            finalTransform = new TransformData(mainMesh.transform);
+            ResetTransform(parentObject);
+
+            Cursor.lockState = CursorLockMode.Locked;
             if (!mainMesh.TryGetComponent<MeshFilter>(out _))
             {
                 mainMesh = GetComponentInChildren<MeshFilter>().gameObject;
             }
 
+            var temMainObj = Instantiate(mainMesh);
+
             var meshComp = mainMesh.GetComponent<MeshFilter>().sharedMesh;
             _midVertex = new Vector3(meshComp.vertices.Average(v => v.x), meshComp.vertices.Average(v => v.y), meshComp.vertices.Average(v => v.z));
-            // var meshBound = meshComp.bounds;
+
+            meshBound = meshComp.bounds;
+            finalScale = meshBound.size;
+            finalScale.Scale(new Vector3(1f / finalDimension.x, 1f / finalDimension.y, 1f / finalDimension.z));
 
             var mainObj = mainMesh;
             if (mainObj.scene.name == null) mainObj = Instantiate(mainMesh);
-            _mainMeshScale = mainObj.transform.localScale;
-            // ResetTransform(mainObj);
+            _mainMeshScale = _mainMeshSlidedSize = mainObj.transform.localScale;
+            _mainMeshSlidedSize.Scale(finalScale);
 
             _slicedMeshes = SliceByDimension(mainObj, finalDimension);
             for (var i = 0; i < _slicedMeshes.Count; i++)
             {
                 _slicedMeshes[i].AddComponent<GearOnSelect>().data = new LocationMeta(Vector3Int.FloorToInt(IndexToVector(i)), Vector3Int.FloorToInt(IndexToVector(i)));
                 _slicedMeshes[i].AddComponent<MeshCollider>().convex = true;
+                _slicedMeshes[i].transform.localPosition = Vector3.zero;
+                _slicedMeshes[i].transform.localRotation = Quaternion.identity;
                 _slicedMeshes[i].transform.parent = parentObject.transform;
             }
-            
+
+            finalTransform.ApplyTo(parentObject.transform);
+            Destroy(temMainObj);
+
             StartCoroutine(StartShuffle());
+            yield break;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SliceThread());
         }
 
         private static void ResetTransform(GameObject obj)
         {
-            obj.transform.localPosition = Vector3.zero;
-            obj.transform.localRotation = Quaternion.identity;
-            obj.transform.localScale = Vector3.one;
+            obj.transform.position = Vector3.zero;
+            obj.transform.rotation = Quaternion.identity;
+            // obj.transform.scale = Vector3.one;
         }
 
         private IEnumerator StartShuffle(float delay = 5)
@@ -146,10 +196,12 @@ namespace Script.Game
                 // var slicedMeshMidVertex = new Vector3(slicedMeshComp.vertices.Average(v => v.x), slicedMeshComp.vertices.Average(v => v.y), slicedMeshComp.vertices.Average(v => v.z));
 
                 var offset = IndexToVector(i);
-                offset.Scale(_mainMeshScale);
-                StartCoroutine(SmoothLerp(_slicedMeshes[i], offset * 5 - _slicedMeshes[i].transform.position, 1f));
+                offset.Scale(_mainMeshSlidedSize);
+                StartCoroutine(SmoothLerp(_slicedMeshes[i], offset, 1f));
                 yield return new WaitForEndOfFrame();
             }
+
+            // yield break; 
 
             _emptySpace = Vector3Int.zero;
             _slicedMeshes[0].SetActive(false);
@@ -176,21 +228,25 @@ namespace Script.Game
             }
         }
 
-        private IEnumerator SmoothLerp(GameObject obj, Vector3 offset, float time)
+        private IEnumerator SmoothLerp(GameObject obj, Vector3 to, float time)
         {
-            var startingPos = obj.transform.position;
-            var finalPos = startingPos + offset;
+            var startingPos = obj.transform.localPosition;
+            print("from " + startingPos.ToString() + " to " + to.ToString());
 
             float elapsedTime = 0;
 
+            var actualPosition = meshBound.size / 2;
+            actualPosition.Scale(_mainMeshScale);
+            actualPosition = to - actualPosition;
+            
             while (elapsedTime < time)
             {
-                obj.transform.position = Vector3.Lerp(startingPos, finalPos, elapsedTime / time);
+                obj.transform.localPosition = Vector3.Lerp(startingPos, actualPosition, elapsedTime / time);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
-            obj.transform.position = finalPos;
+            obj.transform.localPosition = actualPosition;
         }
 
         private int GetObjectIndex(Vector3Int v)
@@ -233,12 +289,15 @@ namespace Script.Game
                 var targetSpaceLocationMeta = _slicedMeshes[GetObjectIndex(targetCoordinate)].GetComponent<GearOnSelect>().data as LocationMeta;
                 (emptySpaceLocationMeta.CurrentLocation, targetSpaceLocationMeta.CurrentLocation) = (targetSpaceLocationMeta.CurrentLocation, emptySpaceLocationMeta.CurrentLocation);
 
-                _slicedMeshes[GetObjectIndex(_emptySpace)].transform.position = _slicedMeshes[GetObjectIndex(targetCoordinate)].transform.position;
+                _slicedMeshes[GetObjectIndex(_emptySpace)].transform.localPosition = _slicedMeshes[GetObjectIndex(targetCoordinate)].transform.localPosition;
 
-                Vector3 offset = _emptySpace - targetCoordinate;
-                offset.Scale(_mainMeshScale);
+                Vector3 to = _emptySpace * 2;
+                to.Scale(_mainMeshSlidedSize);
 
-                StartCoroutine(SmoothLerp(_slicedMeshes[GetObjectIndex(targetCoordinate)], offset * 10, 0.05f));
+                Vector3 offset = targetSpaceLocationMeta.DefaultLocation;
+                offset.Scale(_mainMeshSlidedSize);
+                
+                StartCoroutine(SmoothLerp(_slicedMeshes[GetObjectIndex(targetCoordinate)], to - offset, 0.05f));
                 (_slicedMeshes[GetObjectIndex(targetCoordinate)], _slicedMeshes[GetObjectIndex(_emptySpace)])
                     = ((_slicedMeshes[GetObjectIndex(_emptySpace)], _slicedMeshes[GetObjectIndex(targetCoordinate)]));
 
@@ -256,7 +315,7 @@ namespace Script.Game
         private void Update()
         {
             // no button press
-            if (!Input.GetMouseButtonDown(0))
+            if (!Input.GetMouseButtonDown(0) || gameover)
                 return;
 
             // can't move
@@ -266,7 +325,21 @@ namespace Script.Game
             // is all complete?
             if (_slicedMeshes.All(g => g.TryGetComponent(out GearOnSelect c) && ((LocationMeta)c.data).isDefaultLocation()))
             {
-                print("game over");
+                gameover = true;
+                var emptySpaceLocationMeta = _slicedMeshes[GetObjectIndex(_emptySpace)].GetComponent<GearOnSelect>().data as LocationMeta;
+
+                Vector3 to = _emptySpace * 2;
+                to.Scale(_mainMeshSlidedSize);
+
+                Vector3 offset = emptySpaceLocationMeta.DefaultLocation;
+                offset.Scale(_mainMeshSlidedSize);
+
+                _slicedMeshes[GetObjectIndex(_emptySpace)].transform.position = to - offset;
+                _slicedMeshes[GetObjectIndex(_emptySpace)].SetActive(true);
+                foreach (var t in _slicedMeshes)
+                {
+                    StartCoroutine(SmoothLerp(t, Vector3.zero, 1f));
+                }
             }
         }
     }
